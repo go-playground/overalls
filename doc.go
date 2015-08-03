4a25313fb3611f42f7c68fb7da3f15cf2e06f343
joeybloggs <Dean.Karn@metricaid.com>
2015-08-03 12:24:04 -0400

    add doc.go for godocs

---
+++ doc.go
@@ -0,0 +1,36 @@
+/*
+Package overalls takes multi-package go projects, runs test coverage tests on
+all packages in each directory and finally concatenates into a single file for
+tools like goveralls.
+
+	$ overalls -help
+
+	usage: overalls -project=[path] -covermode[mode] OPTIONS
+
+	overalls recursively traverses your projects directory structure
+	running 'go test -covermode=count -coverprofile=profile.coverprofile'
+	in each directory with go test files, concatenates them into one
+	coverprofile in your root directory named 'overalls.coverprofile'
+
+	OPTIONS
+	  -project
+		Your project path relative to the '$GOPATH/src' directory
+		example: -project=github.com/bluesuncorp/overalls
+
+	  -covermode
+	    Mode to run when testing files.
+	    default:count
+
+	OPTIONAL
+
+	  -ignore
+	    A comma separated list of directory names to ignore, relative to project path.
+	    example: -ignore=[.git,.hiddentdir...]
+	    default: '.git'
+
+	  -debug
+	    A flag indicating whether to print debug messages.
+	    example: -debug
+	    default:false
+*/
+package main
